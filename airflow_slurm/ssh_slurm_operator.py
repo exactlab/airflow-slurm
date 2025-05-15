d9c1d76a94157e7d02f936d92ae5c13adda7595d
Andrea Recchia <andrearecchia92@outlook.it>
2025-05-15 10:45:21 +0200

    adding log - info

--- airflow_slurm/ssh_slurm_operator.py
+++ airflow_slurm/ssh_slurm_operator.py
@@ -129,7 +129,7 @@ class SSHSlurmOperator(BaseOperator):
             )
             stdin.write(slurm_script)
             stdin.channel.shutdown_write()
-            self.log.debug(f"Running sbatch script:\n{slurm_script}")
+            self.log.info(f"Running script:\n{slurm_script}")
             output = stdout.read().decode().strip()
             error = stderr.read().decode().strip()
             self.log.debug(f"{output}")
